857b0e721011028c2cb8cf988d75352033f2410c
Ivan Hurov <ivan.hurov@nure.ua>
2025-05-05 22:22:03 +0300

    deploy

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.status(200).send("hello from backend");
+  res.status(200).send("hello from backend!!!");
 });
 
 // Routes
